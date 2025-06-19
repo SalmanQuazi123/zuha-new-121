9a9f8a073d5fd778c548af34f025f1b3fd2ad9e2
SalmanQuazi123 <salmanquazi90@gmail.com>
2025-06-19 17:38:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginPage from './components/LoginPage'
+import LandingPage from './components/LandingPage'
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LoveLetter.jsx
@@ -0,0 +1,1 @@
+// Placeholder for LoveLetter.jsx
